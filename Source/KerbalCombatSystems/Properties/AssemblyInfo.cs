193f6df6e0ee06c52c8b3ce6385a9c5b755257d3
Lawrence <lawrienash@hotmail.com>
2022-08-04 17:29:58 +0100

    change name

--- Source/KerbalCombatSystems/Properties/AssemblyInfo.cs
+++ Source/KerbalCombatSystems/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StockCombatAI")]
+[assembly: AssemblyTitle("KerbalCombatSystems")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StockCombatAI")]
+[assembly: AssemblyProduct("KerbalCombatSystems")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
