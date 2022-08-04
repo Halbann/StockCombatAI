9c36d5553189de6f29130e0a1e9a3e7aad4fcdf4
Spartwo <38593593+Spartwo@users.noreply.github.com>
2022-08-04 16:05:28 +0100

    Added shitty spartwo source

---
+++ Source/KerbalCombatSystems/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KSP.UI.Screens;
+using UnityEngine;
+using System.IO;
+
+namespace KerbalCombatSystems
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class MissileGuidance : MonoBehaviour
+    {
+        public void Update()
+        {
+            
+        }
+    }
+
+    
+
+   
+
+    public class ModuleMissileGuidance : PartModule
+    {
+        float SafeDistance;
+        float TerminalDistance;
+
+      
+
+        public override void OnStart(StartState state)
+        {
+
+            //check for a ship controller
+            //if found wait
+
+            //once decoupled copy the nearest ship(assume it's parent)'s target vessel
+
+            //eject at slow speed
+            //wait
+            //match velocity
+
+            //do hatbats neato Missile guidance
+        }
+
+        public void Fire()
+        {
+            StartCoroutine(SafeDistanceCoroutine());
+        }
+
+        IEnumerator SafeDistanceCoroutine()
+        {
+            yield return new WaitUntil(true);
+        }
+
+        public void Update()
+        {
+
+        }
+
+
+
+
+    }
+}
