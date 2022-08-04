3cb9a04e690a054bff20813bc7512247af90c0b2
Lawrence <lawrienash@hotmail.com>
2022-08-04 23:42:48 +0100

    added missile guidance :)

--- Source/KerbalCombatSystems/MissileGuidance.cs
+++ Source/KerbalCombatSystems/MissileGuidance.cs
@@ -46,6 +46,11 @@ namespace KerbalCombatSystems
 
         Vessel target;
 
+        LineRenderer targetLine;
+        LineRenderer rvLine;
+        LineRenderer correctionLine;
+        LineRenderer SASLine;
+
         [KSPEvent(guiActive = true,
                   guiActiveEditor = false,
                   guiName = "Fire",
@@ -84,9 +89,45 @@ namespace KerbalCombatSystems
                 engine.Activate();
             }
 
+
             // throttle up
 
             vessel.ctrlState.mainThrottle = 1;
+
+            // Start line renderer
+
+            //lineRenderer = new LineRenderer();
+            //var myLineRenderer = Instantiate(lineRenderer);
+
+            float th = 0.5f;
+
+            targetLine = new GameObject().AddComponent<LineRenderer>();
+            targetLine.useWorldSpace = true;
+            Material lineMaterial = new Material(Shader.Find("Standard"));
+            lineMaterial.color = Color.magenta;
+            targetLine.material = lineMaterial;
+            targetLine.SetWidth(th, th);
+
+            rvLine =  new GameObject().AddComponent<LineRenderer>();
+            rvLine.useWorldSpace = true;
+            Material lineMaterial2 = new Material(Shader.Find("Standard"));
+            lineMaterial2.color = Color.green;
+            rvLine.material = lineMaterial2;
+            rvLine.SetWidth(th, th);
+
+            correctionLine =  new GameObject().AddComponent<LineRenderer>();
+            correctionLine.useWorldSpace = true;
+            Material lineMaterial3 = new Material(Shader.Find("Standard"));
+            lineMaterial3.color = Color.white;
+            correctionLine.material = lineMaterial3;
+            correctionLine.SetWidth(th, th);
+
+            SASLine =  new GameObject().AddComponent<LineRenderer>();
+            SASLine.useWorldSpace = true;
+            Material lineMaterial4 = new Material(Shader.Find("Standard"));
+            lineMaterial4.color = Color.blue;
+            SASLine.material = lineMaterial4;
+            SASLine.SetWidth(th, th);
 
 
             // enable autopilot
@@ -94,19 +135,16 @@ namespace KerbalCombatSystems
             engageAutopilot = true;
         }
 
-        public override void OnFixedUpdate()
-        {
-        }
-
-        public override void OnStart(StartState state)
-        {
-        }
-        public override void OnUpdate()
+        public void FixedUpdate()
         {
             if (engageAutopilot)
             {
-                var fwd = (target.transform.position - vessel.transform.position).normalized;
-                var up = Vector3.Cross(fwd, vessel.orbit.GetOrbitNormal());
+                Vector3 targetVector = (target.transform.position - vessel.transform.position).normalized;
+
+                    Vector3[] linePositions1 = { target.transform.position, vessel.transform.position };
+                    targetLine.SetPositions(linePositions1);
+
+                Vector3 up = Vector3.Cross(targetVector, vessel.orbit.GetOrbitNormal());
 
                 bool sasWasOn = vessel.ActionGroups[KSPActionGroup.SAS];
                 vessel.ActionGroups.SetGroup(KSPActionGroup.SAS, true);
@@ -117,11 +155,33 @@ namespace KerbalCombatSystems
                     vessel.Autopilot.Enable(VesselAutopilot.AutopilotMode.StabilityAssist);
                 }
 
-                Quaternion quat = Quaternion.LookRotation(fwd, up) * Quaternion.Euler(90, 0, 0);
+                Vector3 relVel = vessel.GetObtVelocity() - target.GetObtVelocity();
+                Vector3 relVelNrm = relVel.normalized;
+
+                    Vector3[] linePositions3 = { vessel.transform.position, vessel.transform.position + (relVelNrm * 50) };
+                    rvLine.SetPositions(linePositions3);
+
+                float correctionRatio = (relVel.magnitude / GetCurrentAcceleration(vessel)) * 1.33f;
+
+                Vector3 correction = Vector3.LerpUnclamped(relVelNrm, targetVector, 1 + correctionRatio);
+
+                    Vector3[] linePositions2 = { vessel.transform.position, vessel.transform.position + (correction * 50) };
+                    correctionLine.SetPositions(linePositions2);
+
+                Quaternion quat = Quaternion.LookRotation(correction, up) * Quaternion.Euler(90, 0, 0);
                 Quaternion cquat = vessel.Autopilot.SAS.lockedRotation;
                 Quaternion progressiveRotation = Quaternion.Slerp(cquat, quat, 0.1f);
                 vessel.Autopilot.SAS.LockRotation(progressiveRotation);
+
+                    Vector3[] linePositions4 = { vessel.transform.position, vessel.transform.position + ((progressiveRotation * Vector3.up) * 50) };
+                    SASLine.SetPositions(linePositions4);
             }
+        }
+        public override void OnStart(StartState state)
+        {
+        }
+        public override void OnUpdate()
+        {
         }
 
         ModuleDecouple FindDecoupler(Part origin)
@@ -136,6 +196,13 @@ namespace KerbalCombatSystems
             }
 
             return null;
+        }
+
+        float GetCurrentAcceleration(Vessel v)
+        {
+            List<ModuleEngines> engines = v.FindPartModulesImplementing<ModuleEngines>();
+            float thrust = engines.Sum(e => e.GetCurrentThrust());
+            return thrust / vessel.GetTotalMass();
         }
     }
 }
