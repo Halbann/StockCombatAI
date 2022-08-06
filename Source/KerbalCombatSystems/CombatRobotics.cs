0bae825393b4f81365839d7571640db68f6d4399
Spartwo <38593593+Spartwo@users.noreply.github.com>
2022-08-06 02:31:53 +0100

    KAL 500 Combat Robotics, Part patches

--- Source/KerbalCombatSystems/CombatRobotics.cs
+++ Source/KerbalCombatSystems/CombatRobotics.cs
@@ -3,12 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KSP.UI.Screens;
+using UnityEngine;
+using System.IO;
+using System.Collections;
+using Expansions.Serenity;
 
 namespace KerbalCombatSystems
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     class ModuleCombatRobotics : PartModule
     {
-        //accelerometer
+        private ModuleRoboticController KAL;
+        
+        //access the sequence internals
+        public ModuleRoboticController.SequenceDirectionOptions Forward { get; private set; }
+        public ModuleRoboticController.SequenceLoopOptions Once { get; private set; }
+
+        public virtual void Start()
+        {
+            KAL = part.FindModuleImplementing<ModuleRoboticController>();
+            KAL.SetLength(1);
+            KAL.SetLoopMode(Once);
+            KAL.ToggleControllerEnabled(true);
+            //reset the combat robotics on load
+            PassiveTrigger();
+        }
+
+        //these buttons are just for debugging
+        [KSPEvent(guiActive = true,
+                  guiActiveEditor = false,
+                  guiName = "Trigger Combat Mode")]
+        public void CombatTrigger()
+        {
+            Debug.Log("Extending KAL-500 Position");
+            //trigger robotics to end of sequence(trigger Combat)
+            KAL.SetDirection(Forward);
+            KAL.SequencePlay();
+        }
+
+        //these buttons are just for debugging
+        [KSPEvent(guiActive = true,
+                  guiActiveEditor = false,
+                  guiName = "Trigger Passive Mode")]
+        public void PassiveTrigger()
+        {
+            Debug.Log("Resetting KAL-500 Position");
+            //trigger robotics to start of sequence(trigger Passive)
+            KAL.SetDirection(Forward);
+            //Reverse doens't work as advertised so setting as forward then reversing
+            KAL.ToggleDirection();
+            KAL.SequencePlay();
+        }
+
     }
 }
