016b0ed81a8a08b32f75d7dc85ce0994b640fc38
Spartwo <38593593+Spartwo@users.noreply.github.com>
2022-08-11 01:36:06 +0100

    Update FireworkTargetting.cs

--- Source/KerbalCombatSystems/FireworkTargetting.cs
+++ Source/KerbalCombatSystems/FireworkTargetting.cs
@@ -13,6 +13,37 @@ namespace KerbalCombatSystems
     public class ModuleFirework : PartModule
     {
         
+// Debugging line variables.
+
+        LineRenderer TargetLine, LeadLine;
+        private int RoundBurst;
+
+private IENumerator FireworkAim()
+{
+ 
+            //until aimed
+//recalculate target vector
+
+}
+
+
+public void Start()
+        {
+            Target = part.FindModuleImplementing<ModuleWeaponController>().target;
+            RoundBurst = part.FindModuleImplementing<ModuleWeaponController>().FWRoundBurst;
+            Firer = vessel;
+
+//get list of fireworks
+
+
+StartCoroutine(FireworkAim());
+        }
 
     }
+
+public void OnDestroy()
+        {
+            KCSDebug.DestroyLine(LeadLine);
+            KCSDebug.DestroyLine(TargetLine);
+        }
 }
