129fbeef61bb634630655401c293025bd5b94b79
Spartwo <38593593+Spartwo@users.noreply.github.com>
2022-11-22 10:04:33 +0000

    Fix Object Tracking Module
    Version of script in release branch was from before the decision to make new parts as opposed to overwrite them.
    
    80% of code was redundant, push contains the version from around early October.

--- Source/KerbalCombatSystems/ObjectTracking.cs
+++ Source/KerbalCombatSystems/ObjectTracking.cs
@@ -9,10 +9,9 @@ namespace KerbalCombatSystems
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     class ModuleObjectTracking : PartModule
     {
-        List<ModuleAnimationGroup> SpinAnims;
         //all ore scanning parts plus the sentinal at superlong range
-
         const string objectTrackingGroupName = "Situational Awareness";
+        private int scalingFactor;
 
         [KSPField(
               guiActive = true,
@@ -24,6 +23,9 @@ namespace KerbalCombatSystems
               UI_Label(scene = UI_Scene.All)]
         public float detectionRange = 0f;
 
+        [KSPField(isPersistant = true)]
+        public float baseDetectionRange = 0f;
+
         public override string GetInfo()
         {
             StringBuilder output = new StringBuilder();
@@ -34,83 +36,10 @@ namespace KerbalCombatSystems
             return output.ToString();
         }
 
-        /*public override void OnStart(StartState state)
+        public override void OnStart(StartState state)
         {
-            if (HighLogic.LoadedSceneIsEditor)
-                GameEvents.onEditorVariantApplied.Add(OnVariantApplied);
-
-            SpinAnims = part.FindModulesImplementing<ModuleAnimationGroup>();
-        }*/
-
-        private void OnVariantApplied(Part appliedPart, PartVariant variant)
-        {
-            if (appliedPart != part) return;
-
-            string SensorSize = variant.Name;
-            ModuleResourceScanner OreScanner = part.FindModuleImplementing<ModuleResourceScanner>();
-
-            Debug.Log(SensorSize);
-
-            switch (SensorSize)
-            {
-                case "Medium":
-                    detectionRange = 1000f;
-                    OreScanner.MaxAbundanceAltitude = 500000;//500km
-                    UpdateButtons(SpinAnims[0], SpinAnims[1]);
-                    break;
-                case "Short":
-                    detectionRange = 500f;
-                    OreScanner.MaxAbundanceAltitude = 100000;//100km
-                    UpdateButtons(SpinAnims[1], SpinAnims[0]);
-                    break;
-                /*case "Dome":
-                    detectionRange = 250f * ScalingFactor;
-                    OreScanner.MaxAbundanceAltitude = 30000;//30km
-                    break;*/
-                default:
-                    Debug.Log("variant not found");
-                    //it's a non-variant scanner, no need to modify ranges
-                    break;
-            }
-        }
-
-        private void UpdateButtons(ModuleAnimationGroup EnabledAnim, ModuleAnimationGroup DisabledAnim)
-        {
-            // SpinAnims[0].moduleIsEnabled = SensorSize == "Medium";
-            // SpinAnims[1].moduleIsEnabled = SensorSize == "Short";
-            //SpinAnims[0].deployActionName
-
-            EnabledAnim.enabled = true;
-
-            EnabledAnim.Fields["Deploy <<1>>"].guiActive = true;
-            EnabledAnim.Fields["Retract <<1>>"].guiActive = true;
-            EnabledAnim.Fields["Deploy <<1>>"].guiActiveEditor = true;
-            EnabledAnim.Fields["Retract <<1>>"].guiActiveEditor = true;
-
-
-            /*SpinAnims[0].Events["Deploy <<1>>"].guiActive = SensorSize == "Medium";
-            SpinAnims[0].Events["Retract <<1>>"].guiActive = SensorSize == "Medium";
-            SpinAnims[0].Events["Deploy <<1>>"].guiActiveEditor = SensorSize == "Medium";
-            SpinAnims[0].Events["Retract <<1>>"].guiActiveEditor = SensorSize == "Medium";*/
-
-            //Debug.Log(SpinAnims[0].deployActionName);//returns above values
-            //Debug.Log(SpinAnims[0].retractActionName);//returns above values
-
-            RefreshAssociatedWindows(part);
-        }
-
-        public static void RefreshAssociatedWindows(Part part)
-        {
-            IEnumerator<UIPartActionWindow> window = FindObjectsOfType(typeof(UIPartActionWindow)).Cast<UIPartActionWindow>().GetEnumerator();
-            while (window.MoveNext())
-            {
-                if (window.Current == null) continue;
-                if (window.Current.part == part)
-                {
-                    window.Current.displayDirty = true;
-                }
-            }
-            window.Dispose();
+            scalingFactor = HighLogic.CurrentGame.Parameters.CustomParams<KCSCombat>().ScalingFactor;
+            detectionRange = baseDetectionRange * scalingFactor;
         }
     }
 }
