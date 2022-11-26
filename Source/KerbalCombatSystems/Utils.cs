5e574fada3cf3b2ffd63085692e3ac1e1a14123e
Lawrence <30965946+LawrenceJN@users.noreply.github.com>
2022-11-26 18:10:36 +0000

    replicate util functions moves for clearer diff

--- Source/KerbalCombatSystems/Utils.cs
+++ Source/KerbalCombatSystems/Utils.cs
@@ -6,6 +6,117 @@ namespace KerbalCombatSystems
 {
     public static partial class KCS
     {
+        public static float AveragedSize(Vessel v)
+        {
+            Vector3 size = v.vesselSize;
+            return (size.x + size.y + size.z) / 3;
+        }
+
+        public static float FuelMass(List<Part> parts)
+        {
+            float totalMass = 0;
+            foreach (Part part in parts)
+            {
+                if (part.partInfo.category == PartCategories.Coupling) break;
+                totalMass += part.GetResourceMass();
+            }
+
+            return totalMass;
+        }
+
+        public static float DryMass(List<Part> parts)
+        {
+            float totalMass = 0;
+            foreach (Part part in parts)
+            {
+                if (part.partInfo.category == PartCategories.Coupling) break;
+                totalMass += part.mass;
+            }
+
+            return totalMass;
+        }
+
+        public static float GetMaxAcceleration(Vessel v)
+        {
+            return GetMaxThrust(v) / v.GetTotalMass();
+        }
+
+        public static float GetMaxThrust(Vessel v)
+        {
+            List<ModuleEngines> engines = v.FindPartModulesImplementing<ModuleEngines>();
+            engines.RemoveAll(e => !e.EngineIgnited || !e.isOperational);
+            float thrust = engines.Sum(e => e.MaxThrustOutputVac(true));
+
+            List<ModuleRCS> RCS = v.FindPartModulesImplementing<ModuleRCS>();
+            foreach (ModuleRCS thruster in RCS)
+            {
+                if (thruster.useThrottle)
+                    thrust += thruster.thrusterPower;
+            }
+
+            return engines.Sum(e => e.MaxThrustOutputVac(true));
+        }
+
+        public static string ShorternName(string name)
+        {
+            name = name.Split('(').First();
+            name = name.Split('[').First();
+            name = name.Replace(" - ", " ");
+            name = name.Replace("-class", "");
+            name = name.Replace("Heavy ", "");
+            name = name.Replace("Light ", "");
+            name = name.Replace("Frigate", "");
+            name = name.Replace("Destroyer", "");
+            name = name.Replace("Fighter", "");
+            name = name.Replace("Debris", "");
+            name = name.Replace("Probe", "");
+            name = name.Replace("Lander", "");
+            name = name.Replace("Ship", "");
+            name = name.Replace("Plane", "");
+            name = name.Replace("  ", " ");
+            name = name.Trim();
+
+            return name;
+        }
+
+        public static void TryToggle(bool Direction, ModuleAnimationGroup Animation)
+        {
+            if (Direction && Animation.isDeployed == false)
+            {
+                //try deploy if not already
+                Animation.DeployModule();
+            }
+            else if (!Direction && Animation.isDeployed == true)
+            {
+                //try retract if not already
+                Animation.RetractModule();
+            }
+
+            //do nothing otherwise
+        }
+
+        public static ModuleCommand FindCommand(Vessel craft)
+        {
+            //get a list of onboard control points and return the first found
+            List<ModuleCommand> CommandPoints = craft.FindPartModulesImplementing<ModuleCommand>();
+            if (CommandPoints.Count != 0)
+            {
+                return CommandPoints.First();
+            }
+            //gotta have a command point somewhere so this is just for compiling
+            return null;
+        }
+
+        public static ModuleShipController FindController(Vessel v)
+        {
+            var ship = KCSController.ships.Find(m => m.vessel == v);
+
+            if (ship == null)
+                return v.FindPartModuleImplementing<ModuleShipController>();
+
+            return ship;
+        }
+
         public static Seperator FindDecoupler(Part origin, string type, bool ignoreTypeRequirement)
         {
             Part currentPart;
@@ -53,78 +164,6 @@ namespace KerbalCombatSystems
             return null;
         }
 
-        public static float VesselDistance(Vessel v1, Vessel v2)
-        {
-            return (v1.transform.position - v2.transform.position).magnitude;
-        }
-
-        public static float GetMaxAcceleration(Vessel v)
-        {
-            return GetMaxThrust(v) / v.GetTotalMass();
-        }
-
-        public static float GetMaxThrust(Vessel v)
-        {
-            List<ModuleEngines> engines = v.FindPartModulesImplementing<ModuleEngines>();
-            engines.RemoveAll(e => !e.EngineIgnited || !e.isOperational);
-            float thrust = engines.Sum(e => e.MaxThrustOutputVac(true));
-
-            List<ModuleRCS> RCS = v.FindPartModulesImplementing<ModuleRCS>();
-            foreach (ModuleRCS thruster in RCS)
-            {
-                if (thruster.useThrottle)
-                    thrust += thruster.thrusterPower;
-            }
-
-            return engines.Sum(e => e.MaxThrustOutputVac(true));
-        }
-
-        public static bool RayIntersectsVessel(Vessel v, Ray r)
-        {
-            foreach (Part p in v.parts)
-            {
-                foreach (Bounds b in p.GetColliderBounds())
-                {
-                    if (b.IntersectRay(r)) return true;
-                }
-            }
-
-            return false;
-        }
-
-        public static Vector3 TargetLead(Vessel Target, Part Firer, float TravelVelocity)
-        {
-            Vector3 RelPos = Target.CoM - Firer.transform.position;
-            Vector3 RelVel = Target.GetObtVelocity() - Firer.vessel.GetObtVelocity();
-
-            // Quadratic equation coefficients a*t^2 + b*t + c = 0
-            float a = Vector3.Dot(RelVel, RelVel) - TravelVelocity * TravelVelocity;
-            float b = 2f * Vector3.Dot(RelVel, RelPos);
-            float c = Vector3.Dot(RelPos, RelPos);
-
-            float desc = b * b - 4f * a * c;
-            float ForwardDelta = 2f * c / (Mathf.Sqrt(desc) - b);
-
-            Vector3 leadPosition = Target.CoM + RelVel * ForwardDelta;
-            return leadPosition - Firer.transform.position;
-        }
-
-        public static void TryToggle(bool Direction, ModuleAnimationGroup Animation)
-        {
-            if (Direction && Animation.isDeployed == false)
-            {
-                //try deploy if not already
-                Animation.DeployModule();
-            }
-            else if (!Direction && Animation.isDeployed == true)
-            {
-                //try retract if not already
-                Animation.RetractModule();
-            }
-
-            //do nothing otherwise
-        }
-
         public static List<ModuleDecouple> FindDecouplerChildren(Part Root, string type, bool ignoreTypeRequirement)
         {
             //run through all child parts of the controllers parent for decoupler modules
@@ -168,6 +207,13 @@ namespace KerbalCombatSystems
                 MoI.z.Equals(0) ? float.MaxValue : torque.z / MoI.z);
         }
 
+        public static float AngularVelocity(Vessel v, Vessel t)
+        {
+            Vector3 tv1 = FromTo(v, t);
+            Vector3 tv2 = tv1 + RelVel(v, t);
+            return Vector3.Angle(tv1.normalized, tv2.normalized);
+        }
+
         public static float Integrate(float d, float a, float i = 0.1f, float v = 0)
         {
             float t = 0;
@@ -198,99 +244,41 @@ namespace KerbalCombatSystems
             return (vel * time) + 0.5f * acceleration * Mathf.Pow(time, 2);
         }
 
-        public static float AngularVelocity(Vessel v, Vessel t)
+        public static Vector3 TargetLead(Vessel Target, Part Firer, float TravelVelocity)
         {
-            Vector3 tv1 = FromTo(v, t);
-            Vector3 tv2 = tv1 + RelVel(v, t);
-            return Vector3.Angle(tv1.normalized, tv2.normalized);
+            Vector3 RelPos = Target.CoM - Firer.transform.position;
+            Vector3 RelVel = Target.GetObtVelocity() - Firer.vessel.GetObtVelocity();
+
+            // Quadratic equation coefficients a*t^2 + b*t + c = 0
+            float a = Vector3.Dot(RelVel, RelVel) - TravelVelocity * TravelVelocity;
+            float b = 2f * Vector3.Dot(RelVel, RelPos);
+            float c = Vector3.Dot(RelPos, RelPos);
+
+            float desc = b * b - 4f * a * c;
+            float ForwardDelta = 2f * c / (Mathf.Sqrt(desc) - b);
+
+            Vector3 leadPosition = Target.CoM + RelVel * ForwardDelta;
+            return leadPosition - Firer.transform.position;
         }
 
-        public static string ShorternName(string name)
+        public static float VesselDistance(Vessel v1, Vessel v2)
         {
-            name = name.Split('(').First();
-            name = name.Split('[').First();
-            name = name.Replace(" - ", " ");
-            name = name.Replace("-class", "");
-            name = name.Replace("Heavy ", "");
-            name = name.Replace("Light ", "");
-            name = name.Replace("Frigate", "");
-            name = name.Replace("Destroyer", "");
-            name = name.Replace("Fighter", "");
-            name = name.Replace("Debris", "");
-            name = name.Replace("Probe", "");
-            name = name.Replace("Lander", "");
-            name = name.Replace("Ship", "");
-            name = name.Replace("Plane", "");
-            name = name.Replace("  ", " ");
-            name = name.Trim();
-
-            return name;
+            return (v1.transform.position - v2.transform.position).magnitude;
         }
 
-        public static ModuleCommand FindCommand(Vessel craft)
+        public static bool RayIntersectsVessel(Vessel v, Ray r)
         {
-            //get a list of onboard control points and return the first found
-            List<ModuleCommand> CommandPoints = craft.FindPartModulesImplementing<ModuleCommand>();
-            if (CommandPoints.Count != 0)
+            foreach (Part p in v.parts)
             {
-                return CommandPoints.First();
-            }
-            //gotta have a command point somewhere so this is just for compiling
-            return null;
-        }
-
-        public static ModuleShipController FindController(Vessel v)
-        {
-            var ship = KCSController.ships.Find(m => m.vessel == v);
-
-            if (ship == null)
-                return v.FindPartModuleImplementing<ModuleShipController>();
-
-            return ship;
-        }
-
-        public static float AveragedSize(Vessel v)
-        {
-            Vector3 size = v.vesselSize;
-            return (size.x + size.y + size.z) / 3;
-        }
-
-        public static float FuelMass(List<Part> parts)
-        {
-            float totalMass = 0;
-            foreach (Part part in parts)
-            {
-                if (part.partInfo.category == PartCategories.Coupling) break;
-                totalMass += part.GetResourceMass();
+                foreach (Bounds b in p.GetColliderBounds())
+                {
+                    if (b.IntersectRay(r)) return true;
+                }
             }
 
-            return totalMass;
-        }
-
-        public static float DryMass(List<Part> parts)
-        {
-            float totalMass = 0;
-            foreach (Part part in parts)
-            {
-                if (part.partInfo.category == PartCategories.Coupling) break;
-                totalMass += part.mass;
-            }
-
-            return totalMass;
+            return false;
         }
     }
-
-    /*public class KCSShip
-    {
-        public Vessel v;
-        public float initialMass;
-
-        public KCSShip(Vessel ship, float mass)
-        {
-            v = ship;
-            initialMass = mass;
-        }
-    }*/
 
     public enum Side
     {
@@ -315,11 +303,23 @@ namespace KerbalCombatSystems
         }
     }
 
-    //public static class VesselExtensions
-    //{
-    //    public static Vector3 Velocity(this Vessel v)
-    //    {
-    //        return v.rootPart.Rigidbody.velocity;
-    //    }
-    //}
+    /*public static class VesselExtensions
+    {
+        public static Vector3 Velocity(this Vessel v)
+        {
+            return v.rootPart.Rigidbody.velocity;
+        }
+    }*/
+
+    /*public class KCSShip
+    {
+        public Vessel v;
+        public float initialMass;
+
+        public KCSShip(Vessel ship, float mass)
+        {
+            v = ship;
+            initialMass = mass;
+        }
+    }*/
 }
