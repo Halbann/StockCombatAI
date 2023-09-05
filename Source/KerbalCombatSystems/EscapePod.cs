d0fb327282b1ad7aa04aeed44cd2d006e5792c71
Lawrence <30965946+LawrenceJN@users.noreply.github.com>
2023-09-05 17:43:12 +0100

    Escape pod direction setting, clearance check, thrust reference & fixes

--- Source/KerbalCombatSystems/EscapePod.cs
+++ Source/KerbalCombatSystems/EscapePod.cs
@@ -22,6 +22,7 @@ namespace KerbalCombatSystems
         private KCSFlightController fc;
         private ModuleDecouplerDesignate seperator;
         private Vessel parent;
+        private int parentPartCount;
 
         private readonly int refreshRate = 5;
 
@@ -29,8 +30,10 @@ namespace KerbalCombatSystems
         public const float escapeSpeedMin = 50f;
         public const float escapeSpeedMax = 500f;
 
+        // Escape burn delta-V.
+
         [KSPAxisField(
-            guiName = "Escape Velocity",
+            guiName = "Escape Δv",
             isPersistant = true,
             groupStartCollapsed = false,
             minValue = escapeSpeedMin,
@@ -49,6 +52,33 @@ namespace KerbalCombatSystems
         )]
         public float escapeSpeed = 200f;
 
+
+        // Escape orbital direction.
+
+        [UI_Cycle(
+            stateNames = new string[]
+            {
+                "Normal",
+                "Anti-Normal",
+                "Prograde",
+                "Retrograde"
+            }, 
+            controlEnabled = true,
+            scene = UI_Scene.All
+        )]
+        [KSPField(
+            guiName = "Escape Direction",
+            isPersistant = true,
+            guiActive = true,
+            guiActiveEditor = true,
+            groupName = groupName,
+            groupDisplayName = groupName
+        )]
+        public int escapeDirection = 0;
+        public EscapeDirection Direction =>
+            (EscapeDirection)escapeDirection;
+
+
         #endregion
 
         #region Buttons/Actions
@@ -65,7 +95,11 @@ namespace KerbalCombatSystems
         public void Launch()
         {
             //find decoupler
-            seperator = FindDecoupler(part, "Escape Pod");
+            seperator = FindDecoupler(part, "EscapePod");
+
+            // We could be on the main command pod and the root, in which case the decoupler is a child.
+            if (seperator == null)
+                seperator = FindDecouplerChildren(vessel.rootPart, "EscapePod").FirstOrDefault();
 
             StartCoroutine(EscapeSequence());
         }
@@ -80,15 +114,16 @@ namespace KerbalCombatSystems
 
         #region Main
 
-        public override void OnStart(StartState state)
+        public override void OnStartFinished(StartState state)
         {
-            if (!HighLogic.LoadedSceneIsFlight || escaped)
+            if (!HighLogic.LoadedSceneIsFlight)
                 return;
 
             shipControllerParts = new List<Part>();
 
             // Store a reference to the parent ship.
             parent = vessel;
+            parentPartCount = parent.parts.Count;
 
             // Find ship controllers and add them to our list.
             var controllers = vessel.FindPartModulesImplementing<ModuleShipController>();
@@ -102,6 +137,8 @@ namespace KerbalCombatSystems
         // Continuously check for a connection to the ship controller.
         IEnumerator StatusRoutine()
         {
+            escaped = false;
+
             while (!escaped)
             {
                 for (int i = shipControllerParts.Count - 1; i >= 0; i--)
@@ -123,18 +160,20 @@ namespace KerbalCombatSystems
 
         private IEnumerator EscapeSequence()
         {
-            // Stop checking for connection.
-            //if (statusRoutine != null)
-            //    StopCoroutine(statusRoutine);
-
             // try to pop decoupler
             if (seperator != null)
             {
                 seperator.Separate();
             }
-            else if (vessel == parent)
+
+            yield return new WaitForFixedUpdate(); // Wait for our new vessel to be created.
+
+            ModuleShipController shipController = vessel.FindPartModuleImplementing<ModuleShipController>();
+
+            if (vessel.parts.Count == parentPartCount
+                && shipController != null && shipController.alive)
             {
-                // We didn't find a decoupler and we're still attached to the parent.
+                // We didn't find a decoupler and we're still part of a functional parent warship.
 
                 Debug.Log("[KCS]: Failed to launch escape pod on " + vessel.vesselName);
 
@@ -148,29 +187,58 @@ namespace KerbalCombatSystems
             yield return new WaitForFixedUpdate(); // Wait for our new vessel to be created.
 
             // Transfer as many crew as possible from the parent ship.
-            TransferCrew();
-
-            ModuleCommand command = FindCommand(vessel);
-            if (command)
-                command.MakeReference();
+            if (parent != vessel)
+                TransferCrew();
 
             // Activate engines.
             engines = vessel.FindPartModulesImplementing<ModuleEngines>();
             engines.ForEach(e => e.Activate());
 
+            ModuleCommand command = FindCommand(vessel);
+            if (command)
+            {
+                command.MakeReference();
+                Vector3 propulsionVector = -GetFireVector(engines);
+                AlignReference(command, propulsionVector.normalized);
+            }
+
             // Add flight controller.
             fc = part.gameObject.AddComponent<KCSFlightController>();
+            fc.lerpAttitude = false;
             fc.throttleLerpRate = 100;
             fc.throttle = 1;
-            fc.alignmentToleranceforBurn = 10;
+            fc.alignmentToleranceforBurn = 90;
             fc.attitude = vessel.ReferenceTransform.up;
             fc.Drive();
+
+            fc.Stability(true);
 
             // Deploy parachutes.
             List<ModuleParachute> parachutes = vessel.FindPartModulesImplementing<ModuleParachute>();
             parachutes.ForEach(p => p.Deploy());
 
-            yield return new WaitForSeconds(1f); // Exiting the ship.
+            yield return new WaitForSeconds(0.2f);
+
+            float clearanceTime = Time.time;
+            if (vessel != parent)
+            {
+                Ray ray = new Ray();
+                bool blocked = true;
+
+                Vector3 escapeDirection = GetEscapeDirection(Planetarium.GetUniversalTime());
+
+                while (blocked && Time.time - clearanceTime < 2)
+                {
+                    yield return new WaitForSeconds(0.1f);
+
+                    ray.origin = vessel.CoM;
+                    ray.direction = escapeDirection;
+
+                    blocked = RayIntersectsVessel(parent, ray);
+                }
+            }
+
+            fc.Stability(false);
 
             StartCoroutine(FlightRoutine());
         }
@@ -186,24 +254,24 @@ namespace KerbalCombatSystems
                 if (!InControl())
                     break;
 
-                if (CheckOrbitUnsafe())
+                if (Direction != EscapeDirection.Retrograde && CheckOrbitUnsafe())
                 {
                     Orbit o = vessel.orbit;
                     double UT;
 
                     // Execute a burn to circularize our orbit at the current altitude.
-                    Vector3d fvel, deltaV = Vector3d.up * 100;
+                    Vector3d fvel, deltaVd = Vector3d.up * 100;
 
-                    while (deltaV.magnitude > 2 && Time.time - lastUpdate < refreshRate)
+                    while (deltaVd.magnitude > 2 && Time.time - lastUpdate < refreshRate)
                     {
                         yield return new WaitForFixedUpdate();
 
                         UT = Planetarium.GetUniversalTime();
                         fvel = Math.Sqrt(o.referenceBody.gravParameter / o.GetRadiusAtUT(UT)) * o.Horizontal(UT);
-                        deltaV = fvel - vessel.GetObtVelocity();
+                        deltaVd = fvel - vessel.GetObtVelocity();
 
-                        fc.attitude = deltaV.normalized;
-                        fc.throttle = Mathf.Lerp(0, 1, (float)(deltaV.magnitude / 10));
+                        fc.attitude = deltaVd.normalized;
+                        fc.throttle = Mathf.Lerp(0, 1, (float)(deltaVd.magnitude / 10));
                         fc.Drive();
                     }
                 }
@@ -212,19 +280,34 @@ namespace KerbalCombatSystems
                     // Plane change. Burning either north or south until we reach the escape speed.
 
                     double UT = Planetarium.GetUniversalTime();
-                    Vector3 orbitNormal = vessel.orbit.Normal(UT);
-                    bool facingNorth = Vector3.Angle(vessel.ReferenceTransform.up, orbitNormal) < 90;
-                    Vector3 deltaV = orbitNormal * (facingNorth ? 1 : -1) * escapeSpeed;
+                    Vector3 deltaV = GetEscapeDirection(UT) * escapeSpeed;
+
+                    CelestialBody SOI = vessel.orbit.referenceBody;
+                    Vector3 previousDirection = deltaV.normalized;
+                    float directionChange = 0;
+
                     fc.throttle = 1;
 
-                    while (deltaV.magnitude > 10 && InControl())
+                    while (deltaV.magnitude > 10 && InControl() && directionChange < 90)
                     {
+
                         UT = Planetarium.GetUniversalTime();
-                        deltaV = vessel.orbit.Normal(UT) * (facingNorth ? 1 : -1) * deltaV.magnitude;
+                        deltaV = GetEscapeDirection(UT) * deltaV.magnitude;
                         deltaV -= Vector3.Project(vessel.acceleration, deltaV) * TimeWarp.fixedDeltaTime;
 
                         fc.attitude = deltaV.normalized;
                         fc.Drive();
+
+                        if (vessel.orbit.referenceBody != SOI)
+                        {
+                            directionChange = 0;
+                            previousDirection = deltaV.normalized;
+                            SOI = vessel.orbit.referenceBody;
+                        }
+
+                        // Limit the change in direction so that we don't loop around in a very low gravity SOI.
+                        directionChange += Vector3.Angle(previousDirection, deltaV.normalized);
+                        previousDirection = deltaV.normalized;
 
                         yield return new WaitForFixedUpdate();
                     }
@@ -239,7 +322,17 @@ namespace KerbalCombatSystems
             }
 
             // Remove the flight controller and allow the guidance to cease.
+
             Destroy(fc);
+
+            // Let the the aero and parachutes take over if we're de-orbiting.
+
+            yield return new WaitUntil(() => fc == null);
+
+            if (Direction == EscapeDirection.Retrograde)
+            {
+                vessel.ActionGroups.SetGroup(KSPActionGroup.SAS, false);
+            }
         }
 
         bool InControl()
@@ -252,6 +345,32 @@ namespace KerbalCombatSystems
         #endregion
 
         #region Functions
+
+        public enum EscapeDirection
+        {
+            Normal,
+            AntiNormal,
+            Prograde,
+            Retrograde
+        }
+
+        private Vector3 GetEscapeDirection(double UT)
+        {
+            switch (Direction)
+            {
+                case EscapeDirection.Normal:
+                    //return vessel.orbit.Normal(UT).normalized;
+                    return (Vector3)vessel.orbit.h.xzy.normalized;
+                case EscapeDirection.AntiNormal:
+                    return (Vector3)vessel.orbit.h.xzy.normalized * -1;
+                case EscapeDirection.Prograde:
+                    return (Vector3)vessel.orbit.Prograde(UT).normalized;
+                case EscapeDirection.Retrograde:
+                    return (Vector3)vessel.orbit.Prograde(UT).normalized * -1;
+                default:
+                    return (Vector3)vessel.orbit.h.xzy.normalized;
+            }
+        }
 
         private bool CheckOrbitUnsafe()
         {
