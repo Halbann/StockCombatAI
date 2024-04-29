7f4f94ae45c4854cc61807cf101c534003c60944
Lawrence <30965946+LawrenceJN@users.noreply.github.com>
2024-04-29 01:04:13 +0100

    Renamed save settings + easier access.

---
+++ Source/KerbalCombatSystems/SaveSettings.cs
@@ -0,0 +1,38 @@
+﻿namespace KerbalCombatSystems
+{
+    public class KCSSaveSettings : GameParameters.CustomParameterNode
+    {
+        // Instance.
+
+        private static KCSSaveSettings instance;
+        public static KCSSaveSettings Instance
+        {
+            get
+            {
+                if (instance == null)
+                    if (HighLogic.CurrentGame != null)
+                        instance = HighLogic.CurrentGame.Parameters.CustomParams<KCSSaveSettings>();
+
+                return instance;
+            }
+        }
+
+
+        // Boilerplate.
+
+        public override string Section => "KCS";
+        public override string DisplaySection => "KCS";
+        public override int SectionOrder => 1;
+        public override string Title => "Combat";
+        public override GameParameters.GameMode GameMode => GameParameters.GameMode.ANY;
+        public override bool HasPresets => false;
+
+
+        // Custom parameters.
+
+        public static bool AllowWithdrawalRef => Instance.allowWithdrawal;
+        [GameParameters.CustomParameterUI("Allow Withdrawal",
+            toolTip = "Are ships ever allowed to retreat from combat?")]
+        public bool allowWithdrawal = true;
+    }
+}
